f36d33d7e01b76ba34064f72263d937e6f7267a9
ebamboe <StepashkaV98@ukr.net>
2026-05-16 23:22:18 -0700

    Homework

---
+++ task2.py
@@ -0,0 +1,9 @@
+def multiplication_or_sum(num1, num2):
+    product = num1 * num2
+
+    if product <=1000:
+     return product
+    else:
+     return num1 + num2
+result = multiplication_or_sum(30, 40)
+print("The result is", result)
