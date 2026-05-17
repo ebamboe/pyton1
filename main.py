7937a56f46e704d24ae47763e46a7109235536ac
Vlad Stepanenko <vladstepanenko@Vlads-MacBook-Air.local>
2026-05-16 22:42:53 -0700

    Task 1

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def multiplication_or_sum(num1, num2):
+    product = num1 * num2
+
+    if product <=1000:
+     return product
+    else:
+     return num1 + num2
+result = multiplication_or_sum(20, 30)
+print("The result is", result)
