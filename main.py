2109682e0e69b14632fca61fd80f619a71339cf3
ebamboe <StepashkaV98@ukr.net>
2026-05-18 19:22:24 -0700

    dothis

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+age = int(input("enter your age: "))
+
+if age  > 20 and 30 : 
+    print("age is berween 20 and 30")
+elif age > 30 and age < 40:
+    print("age is between 30 and 40")
+elif age > 40 and age < 50:
+    print("age is between 40 and 50")    
+else:
+    print("age is less than 20 or greater than 50")
