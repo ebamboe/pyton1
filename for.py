7f9f934e7500cd8f12a7f8d2d82b85fc8e68250a
ebamboe <StepashkaV98@ukr.net>
2026-05-25 17:48:01 -0700

    for

---
+++ for.py
@@ -0,0 +1,30 @@
+# for x in range(10):
+#    print(x) 
+
+# fruits = ["apple", "banana", "cherry"]
+
+# for x in fruits:
+#      print(x) #виводить кожен фрукт
+#      if x == "banana": #якщо фрукт банан, то завершує цикл
+#        break
+
+
+# numbers = [1, 2, 3, 4, 5]
+# totalSum = 0
+# for number in numbers:
+#     totalSum += number
+
+# print (totalSum)
+
+#  приклад циклу суми всіх чисел 
+
+
+numbers = [1, 2, 3, 4, 5]
+totalSum = 1
+for number in numbers:
+    if number % 2 == 0:
+        totalSum += number
+
+print (totalSum)
+
+# приклад суми 
